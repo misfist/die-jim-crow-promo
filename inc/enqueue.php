74d4366854bdf02c36c0de8aa60b03a0ac064a24
Pea Lutz <pea@glocal.coop>
2016-04-29 23:15:38 -0400

    Added custom styles to password form.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -5,11 +5,16 @@
  * @package Die_Jim_Crow
  */
 
-add_action( 'wp_enqueue_scripts', 'djc_enqueue_styles' );
-
 function djc_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 }
+
+add_action( 'wp_enqueue_scripts', 'djc_enqueue_styles' );
+
+function djc_enqueue_login_styles() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/admin.css' );
+}
+
+add_action( 'login_enqueue_scripts', 'djc_enqueue_login_styles' );
 
 ?>
